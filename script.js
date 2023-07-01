cffdcfe228545cb670e1a429cf5ef2e37a2878ee
Glitch (flashcardss) <none>
2023-07-01 06:41:35 +0000

    🕗⛩️ Updated with Glitch

--- script.js
+++ script.js
@@ -3,3 +3,39 @@
 
 // prints "hi" in the browser's dev tools console
 console.log("hi");
+var spanishWord =[{english:"Apple",spanish:"Manzana"},
+                  {english:"Red",spanish:"Rojo"},
+                  {english:"Carrot",spanish:"Zanahoria"},
+                  {english:"Orange",spanish:"Naranja"},
+                  {english:"Cheese",spanish:"Queso"},
+                  {english:"Yellow",spanish:"Amarillo"},
+                  {english:"Frog",spanish:"Sapo"},
+                  {english:"Green",spanish:"Verde"},
+                  {english:"Water",spanish:"Agua"},
+                  {english:"Blue",spanish:"Azul"},
+                  {english:"Grapes",spanish:"Uvas"},
+                  {english:"Purple",spanish:"Morado"},
+                  {english:"Pig",spanish:"Cerdo"},
+                  {english:"Pink",spanish:"Rosa"},
+                  {english:"Bat",spanish:"Murcielago"},
+                  {english:"Black",spanish:"Negro"},
+                  {english:"Egg",spanish:"Huevo"},
+                  {english:"White",spanish:"Blanco"},
+                  {english:"Bear",spanish:"Oso"},
+                  {english:"Brown",spanish:"Cafe"},
+                  {english:"Elephant",spanish:"Elefante"},
+                  {english:"Gray",spanish:"Gris"},
+                  {english:"Ice Cream",spanish:"Helado"},
+                  {english:"Cookie",spanish:"Galleta"},
+                  {english:"Cake",spanish:"Pastel"}]
+
+var words
+function showAnswer() {
+document.getElementById("flash-card").innerHTML = words.spanish
+  
+}
+
+function newCard() {
+words=spanishWord[Math.floor(Math.random() * spanishWord.length)]
+document.getElementById("flash-card").innerHTML = words.english
+}
